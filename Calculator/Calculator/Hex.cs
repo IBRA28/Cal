addfc5a34db4a2056cb9433502e982f7209c4321
Hecmatyar <dmitriy.kushnir@inbox.ru>
2017-11-28 17:58:06 +0200

    исправил

--- Calculator/Calculator/Hex.cs
+++ Calculator/Calculator/Hex.cs
@@ -8,25 +8,35 @@ namespace Calculator
 {
     public class Hex
     {
-        public static string Amount(string a, string b)
+        private static string Amount(string a, string b)
         {
             return (Convert.ToInt16(a, 16) + Convert.ToInt16(b, 16)).ToString("X");
         }
-        public static string Substration(string a, string b)
+        private static string Substration(string a, string b)
         {
             return (Convert.ToInt16(a, 16) - Convert.ToInt16(b, 16)).ToString("X");
         }
-        public static string Multiplication(string a, string b)
+        private static string Multiplication(string a, string b)
         {
             return (Convert.ToInt16(a, 16) * Convert.ToInt16(b, 16)).ToString("X");
         }
-        public static string Division(string a, string b)
+        private static string Division(string a, string b)
         {
             return (Convert.ToInt16(a, 16) / Convert.ToInt16(b, 16)).ToString("X");
         }
-        public static string Mod(string a, string b)
+        private static string Mod(string a, string b)
         {
             return (Convert.ToInt16(a, 16) % Convert.ToInt16(b, 16)).ToString("X");
+        }
+        public static string Equal(string source)
+        {
+            var couple = source.Split('+', '*', '/', '-', '%');
+            couple = couple.Select(a => a.Trim()).ToArray();
+            if (source.Contains("+")) return Amount(couple[0], couple[1]);
+            else if (source.Contains("-")) return Substration(couple[0], couple[1]);
+            else if (source.Contains("*")) return Multiplication(couple[0], couple[1]);
+            else if (source.Contains("/")) return Division(couple[0], couple[1]);
+            return Mod(couple[0], couple[1]);
         }
     }
 }
