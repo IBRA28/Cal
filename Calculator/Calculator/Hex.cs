f3b1d3afe3e98ba8a45fe9f43d1bfc2aa2ea869e
Hecmatyar <dmitriy.kushnir@inbox.ru>
2017-11-28 17:22:35 +0200

    Hex
    вроде должно работать

---
+++ Calculator/Calculator/Hex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Hex
+    {
+        public static string Amount(string a, string b)
+        {
+            return (Convert.ToInt16(a, 16) + Convert.ToInt16(b, 16)).ToString("X");
+        }
+        public static string Substration(string a, string b)
+        {
+            return (Convert.ToInt16(a, 16) - Convert.ToInt16(b, 16)).ToString("X");
+        }
+        public static string Multiplication(string a, string b)
+        {
+            return (Convert.ToInt16(a, 16) * Convert.ToInt16(b, 16)).ToString("X");
+        }
+        public static string Division(string a, string b)
+        {
+            return (Convert.ToInt16(a, 16) / Convert.ToInt16(b, 16)).ToString("X");
+        }
+        public static string Mod(string a, string b)
+        {
+            return (Convert.ToInt16(a, 16) % Convert.ToInt16(b, 16)).ToString("X");
+        }
+    }
+}
